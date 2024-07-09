5cf84eba9c0c1f3043645c3ddf2b5600bed6e5ba
Aryan Jagarwal <aryanjagarwal@Aryans-MacBook-Air.local>
2024-07-09 09:37:15 +0530

    done with localStorage version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import Manager from './components/Manager'
 import Navbar from "./components/Navbar"
 
@@ -7,7 +8,12 @@ function App() {
   return (
     <>
      <Navbar/>
-     <Manager/>
+
+     <div className='bg-green-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+      <Manager/>
+     </div>
+     
+     <Footer/>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,17 @@ const Navbar = () => {
                     Pass
                     <span className='text-green-500'>WORLDS/&gt; </span>
                 </div>
-                <ul className=''>
+                {/*<ul className=''>
                     <li className='flex gap-4'>
                         <a className='hover:font-bold' href="/">Home</a>
                         <a className='hover:font-bold' href="#">Contact</a>
                         <a className='hover:font-bold' href="#">About</a>
                     </li>
-                </ul>
+                </ul> */}
+                <button className='text-white bg-green-700 my-5 rounded-full flex justify-between items-center ring-white ring-1'>
+                    <img className='invert w-8 p-1' src="/icons/github.png" alt="github" />
+                    <span className='font-bold px-2'>Github</span>
+                </button>
             </div>
         </nav>
     )
